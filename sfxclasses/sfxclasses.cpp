9d4136f9cff26bd1b4878d1b1a13aa78928e9f62
alex_s_ander < @homepc28052009.filial16.local>
2016-03-24 16:20:30 +0300

    next commit

--- sfxclasses/sfxclasses.cpp
+++ sfxclasses/sfxclasses.cpp
@@ -20,38 +20,6 @@
 
 using namespace std;
 
-//struct proba
-//{
-//	int number;
-//	vector< proba > pvec;
-//	proba(int i )
-//	{ number = i; };
-//};
-//
-//proba* rerucobh( int n, vector< proba >* p  )
-//{
-//	static proba* fi = 0;
-//
-//	for(int i =0; i < p->size(); i ++ )
-//	{
-//		if ( n == p->at( i ).number )
-//		{	
-//			fi = &p->at(i);
-//			return fi;
-//		}
-//		else
-//			rerucobh( n, &p->at(i).pvec );
-//	}
-//	return fi;
-//}
-
-void func( vector<string >* pv )
-{
-	pv->push_back( "one" );
-	pv->push_back("two");
-}
-
-
 int _tmain(int argc, _TCHAR* argv[])
 {
 	setlocale( LC_ALL, ".ACP" );
@@ -59,7 +27,6 @@ int _tmain(int argc, _TCHAR* argv[])
 	charstream cs;
 	TokenizerCmd tc( cs );
 	tc.RunTokenizer();
-	
 	
 }
 
